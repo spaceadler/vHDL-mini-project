9735a9de72b09f5a65ebf9c607314ded522bd714
spaceadler <host.directly413@aleeas.com>
2026-06-15 18:55:41 +0300

    Updated full-adder logic as well as enhancing comments.

--- sanity_check.vhd
+++ sanity_check.vhd
@@ -3,37 +3,47 @@ use ieee.std_logic_1164.all;
 
 entity sanity_check is
     port (
-        i_a         :   in std_logic;               -- a is the first input.
-        i_b         :   in std_logic;               -- b is the second input.
-        i_cin       :   in std_logic;               -- cin is the carry from the previous operation (full adder.)
+        i_a         :   in std_logic;                           -- a is the first input.
+        i_b         :   in std_logic;                           -- b is the second input.
+        i_cin       :   in std_logic;                           -- cin is the carry from the previous theoretical operation.
 
-        o_s         :   out std_logic;              -- output of the operation.
-        o_cout      :   out std_logic);             -- whether if there's a carry bit or not.
+        o_s         :   out std_logic;                          -- sum
+        o_cout      :   out std_logic;                          -- and carry.
+        o_cout_s_a  :   out std_logic);                         -- and operation on the sum and carry.
 
 end entity sanity_check;
 
 architecture hardware of sanity_check is
 
-    signal w_ab_x       :   std_logic;              -- transfer wires for a xor b.
-    signal w_ab_a       :   std_logic;              -- transfer wires for a and b.
-    signal w_ab_cin_a   :   std_logic;              -- transfer wires for o_ab_x and i_cin.
+    signal w_a          :   std_logic;                          -- the idea is that when the cable is not connected, it's in a floating state.
+    signal w_b          :   std_logic;                          -- in which, it's set to 1, which is the same value when it's connected to vcc.
+    signal w_cin        :   std_logic;                          -- so we invert that, to make sure it's ONLY "1" when it's connected to gnd.
+ 
+    signal w_ab_x       :   std_logic;                          -- transfer wires for a xor b.
+    signal w_ab_a       :   std_logic;                          -- transfer wires for a and b.
+    signal w_abx_c_a    :   std_logic;                          -- transfer wires for (a xor b) and i_cin.
 
-    signal w_a          :   std_logic;              -- purely aesthetic,
-    signal w_b          :   std_logic;              -- just here to switch the
-    signal w_cin        :   std_logic;              -- signal with a future "not".
+    signal w_s          :   std_logic;                          -- carry signal for the sum,
+    signal w_cout       :   std_logic;                          -- as well as the carry bit.
 
 begin
 
-    w_a         <= not i_a;                         -- the idea is that when the cable is not connected, it's in a floating state.
-    w_b         <= not i_b;                         -- in which, it's set to 1, which is the same value when it's connected to vcc.
-    w_cin       <= not i_cin;                       -- so we invert that, to make sure it's ONLY "1" when it's connected to something, aka gnd.
+    w_a         <= not      i_a;                                -- apply line 18.
+    w_b         <= not      i_b;                                -- apply line 19.
+    w_cin       <= not      i_cin;                              -- apply line 20.
+ 
+    w_ab_x      <= w_a      xor w_b;                            -- apply line 22.
+    w_ab_a      <= w_a      and w_b;                            -- apply line 23.
+    w_abx_c_a   <= w_ab_x   and w_cin;                          -- apply line 24.
 
+    w_s         <= w_ab_x   xor w_cin;                          -- apply line 26.
+    w_cout      <= w_ab_a   or  w_abx_c_a;                      -- apply line 27.
 
-    w_ab_x      <= w_a      xor w_b;                -- set the value of transfer wire per line 21.
-    w_ab_a      <= w_a      and w_b;                -- set the value of transfer wire per line 22.
-    w_ab_cin_a  <= w_ab_x   and w_cin;              -- set the value of transfer wire per line 23.
-
-    o_cout      <= not (w_ab_a   or  w_ab_cin_a);   -- do a little not for the LED's...
-    o_s         <= not (w_ab_x   xor w_cin);        -- ...this way, they're ON only when "initiated".
-
+    o_s         <= not (w_s     and not (w_cout and w_s));      -- LED is on only if sum is 1 but carry is 0.   PIN_3
+    o_cout      <= not (w_cout  and not (w_cout and w_s));      -- LED is on only if sum is 0 but carry is 1.   PIN_7
+    o_cout_s_a  <= not (w_cout  and w_s);                       -- LED is on only if sum is 1 and carry is 1.   PIN_9
 end architecture hardware;
+
+-- Oh my god this took so much time to write and make sure is correct, the amount of times I wrote this
+-- with just "o_" only to change to "w_" wires, add "i_" wires, as well as fixing logic, rewriting to
+-- make easier to read as well as write this documentation is just astronomical. 
